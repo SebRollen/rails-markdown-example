ceb863661180b71532f250d804eb965d02d86dd2
Sebastian Rollen <seb@rollen.io>
2023-01-30 17:26:20 -0500

    change layout based on path

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,22 @@
 
 class PagesController < ApplicationController
   include HighVoltage::StaticPage
+  layout :layout_for_page
 
   private
+
+  def resource
+    params[:id].split('/').first
+  end
+
+  def layout_for_page
+    case resource
+    when 'blog'
+      'pages/blog_post'
+    else
+      'application'
+    end
+  end
 
   def page_finder_factory
     PageFinder
